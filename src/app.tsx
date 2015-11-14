fd21cea7b42d83585e3e82f703c23b38d10e30ac
uzimith <uzimith.x9@gmail.com>
2015-11-15 08:45:46 +0900

    init

---
+++ src/app.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import Counter from './components/Counter';
+
+const container = document.querySelector("#container");
+
+ReactDOM.render(<Counter />, container);
---
+++ src/components/Counter.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+
+interface State {};
+
+interface Props {
+  increment: React.MouseEventHandler;
+  incrementIfOdd: React.MouseEventHandler;
+  incrementAsync: React.MouseEventHandler;
+  decrement: React.MouseEventHandler;
+  counter: number;
+};
+
+export default class Counter extends React.Component<Props, State> {
+
+  constructor () {
+    super();
+  }
+
+  render () {
+    const { increment, incrementIfOdd, incrementAsync, decrement, counter } = this.props
+    return (
+        <p>
+          Clicked: {counter} times
+          {' '}
+          <button onClick={increment}>+</button>
+          {' '}
+          <button onClick={decrement}>-</button>
+          {' '}
+          <button onClick={incrementIfOdd}>Increment if odd</button>
+          {' '}
+          <button onClick={incrementAsync}>Increment async</button>
+        </p>
+      );
+  }
+}
